2f7d79ee0c0470897251b2163c797a05d9f056d8
sebysira <seby.siragusa@gmail.com>
2015-03-13 10:46:58 +0100

    add c code

--- ns2_project.tcl
+++ ns2_project.tcl
@@ -9,22 +9,22 @@ set val(ifqlen)         50                         ;# max packets in ifq
 set val(rxPower)        1.11                      ;#  (in W)
 set val(txPower)        1.11                      ;#  (in W)
 set val(energymodel)    EnergyModel               ;# 
-set val(initialenergy)  10                        ;#  (in Joule)
+set val(initialenergy)  100                       ;#  (in Joule)
 set val(sleeppower)     0.02                      ;# energia consumata in stato di sleep
 set val(tp)             0.03                      ;# Energia consumata per la transizione dallo stato di sleep a quello di attivita'...
 #set val(ifq)            Queue/DropTail/PriQueue    ;# interface queue type
 
 # DumbAgent no routing!, AODV, DSDV, DSR 
-set val(rp)	DSDV; # 
+set val(rp)	DSR; # 
 if { $val(rp) == "DSR" } {
 set val(ifq)            CMUPriQueue
 } else {
 set val(ifq)            Queue/DropTail/PriQueue
 }
 
-set val(x)	800
-set val(y)	800
-set val(nn)	3				   ;# number of nodes
+set val(x)	1000
+set val(y)	1000
+set val(nn)	20				   ;# number of nodes
 
 # Create simulator
 set ns [new Simulator]
@@ -67,7 +67,7 @@ $ns node-config -adhocRouting $val(rp) \
  				-txPower $val(txPower)
 
 # Create mobile nodes
-for {set i 0} {$i<3} {incr i} {
+for {set i 0} {$i<$val(nn)} {incr i} {
 	set node($i) [$ns node]
 	# disable random motion for static network
 	$node($i) random-motion 1
@@ -77,24 +77,36 @@ for {set i 0} {$i<3} {incr i} {
 set udp(0) [new Agent/UDP]
 $ns attach-agent $node(0) $udp(0)
 set null(0) [new Agent/Null]
-$ns attach-agent $node(2) $null(0)
+$ns attach-agent $node(13) $null(0)
 set cbr(0) [new Application/Traffic/CBR]
 $cbr(0) set packetSize_ 1000
-$cbr(0) set interval_ 4.0
+$cbr(0) set interval_ 0.5
 $cbr(0) set random_ 1
 $cbr(0) set maxpkts_ 10000
 $cbr(0) attach-agent $udp(0)
 
 $ns connect $udp(0) $null(0)
-$ns at 20.0 "$cbr(0) start"
+$ns at 1.0 "$cbr(0) start"
 
 # Define node initial position in nam
-for {set i 0} {$i < 3} {incr i} {
+for {set i 0} {$i < $val(nn)} {incr i} {
 	$ns initial_node_pos $node($i) 30
 }
 
+$ns at 0 "load_perc"
 # Tell ns/nam the simulation stop time
-$ns at 100.0 "finish"
+$ns at 400.0 "finish"
+
+proc load_perc {} {
+        global node val ns
+        set fp [open "percentage1.txt" "r"]
+        
+        for {set i 0} {$i < $val(nn)} {incr i} {
+                set line [gets $fp]
+                $ns at 0 "[$node($i) set ragent_] malicious $line"
+        }
+        close $fp
+}
 
 proc finish {} {
         global ns f nf
